cb89267297421d19ae3fff49a7d795125805a65f
KanekoFutaba <fkaneko88@gmail.com>
2025-08-07 14:16:25 +0900

    facker よるダミーデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,9 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Article.create({ title: "新しい記事です", content: "素晴らしい記事です" })
-Article.create({ title: "新しい記事パート2です", content: "2つ目の記事です" })
+10.times do
+  Article.create(
+    title: Faker::Lorem.sentence(word_count: 5),
+    content: Faker::Lorem.sentence(word_count: 100)
+  )
+end
